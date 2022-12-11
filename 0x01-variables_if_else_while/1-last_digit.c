4a1c46c5fe8e4a48f1662a2870b2bd316d95cdfd
hawau001 <hawauajoke1234@gmail.com>
2022-12-11 08:26:07 -0800

    task2

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -8,14 +8,13 @@
  *
  * Return: Always 0.
  */
-
 int main(void)
 {
+	int n;
 
-	int n;
 
 	srand(time(0));
-	n  =  rand()  -  RAND_MAX / 2;
+	n  =  rand() - RAND_MAX / 2;
 
 	if ((n % 10 ) > 5)
 	{
@@ -31,8 +30,8 @@ int main(void)
 	{
 		printf("Last digit of %d is %d and is 0\n",
 	n, n % 10);
-	
 	}
 
-	return (0);
+	return(0);
+
 }
