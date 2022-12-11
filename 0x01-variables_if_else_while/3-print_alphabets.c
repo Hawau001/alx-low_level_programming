eb1bbf9567d50369e1ccde211e8dad4f00696638
hawau001 <hawauajoke1234@gmail.com>
2022-12-11 13:05:14 -0800

    3-print_alphabets

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,4 +1,3 @@
-
 #include <stdio.h>
 
 /**
@@ -8,16 +7,16 @@
  */
 int main(void)
 {
-    char letter;
+	char letter;
 
-    for (letter = 'a'; letter <= 'z'; letter++)
-        putchar(letter);
+	for (letter = 'a'; letter <= 'z'; letter++)
+		putchar(letter);
 
-    for (letter = 'A'; letter <= 'Z'; letter++)
-        putchar(letter);
+	for (letter = 'A'; letter <= 'Z'; letter++)
+		putchar(letter);
 
-    putchar('\n');
+	putchar('\n');
 
-    return (0);
+	return (0);
 }
 
