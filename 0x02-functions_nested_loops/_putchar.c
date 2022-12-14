44c70ab135ba7f43f608e59b125b1fc0118aed07
hawau001 <hawauajoke1234@gmail.com>
2022-12-13 21:01:11 -0800

    loop

--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -1,11 +1,9 @@
 #include "main.h"
 #include <unistd.h>
 
-
 /**
  * _putchar - write the character c to stdout
  * @c: the character to print
- *
  * Return: On success 1
  */
 int __putchar(char c)
