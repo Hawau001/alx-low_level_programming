b1db73d16fab526d6e8d3274112083ee66b8ce6f
hawau001 <hawauajoke1234@gmail.com>
2022-12-17 22:10:08 -0800

    11-print_to_98.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,21 +1,19 @@
 #include "main.h"
-
 /**
-* print_alphabet_x10 - print 10 times, alphabet in lowercase,
-* followed by new line
+* print_alphabet_x10 - repeats the print_alphabet 10 times.
+* Return: Nothing.
 */
-void print_alphabet_iix10(void)
-
+void print_alphabet_x10(void)
 {
-	int count = 0;
-	char letter;
+	int i;
+	char c;
 
-	while (count++ <= 9)
+for (i = 0; i < 10; ++i)
 	{
-
-		for (letter = 'a'; letter <= 'z'; letter++)
-		_putchar (letter);
-	_putchar ('\n');
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+		_putchar('\n');
 	}
 }
-
