7c2672a95d8307cf19b5634a7d94dc00349316f6
hawau001 <hawauajoke1234@gmail.com>
2022-12-22 17:46:31 -0800

    100-rot13.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -4,44 +4,26 @@
  * rot13 - Encodes a string using rot13.
  * @str: The string to be encoded.
  *
- * Return: A pointer to the encoded string.
+ * Return: address of s
  */
-char *rot13(char *str)
+char *rot13(char *s)
 {
-  int indx1 = 0, indx2;
-  char alphabet[52] = {'A', 'B', 'C', 'D', 'E', 'F',
-		       'G', 'H', 'I', 'J', 'K', 'L',
-		       'M', 'N', 'O', 'P', 'Q', 'R',
-		       'S', 'T', 'U', 'V', 'W', 'X',
-		       'Y', 'Z', 'a', 'b', 'c', 'd',
-		       'e', 'f', 'g', 'h', 'i', 'j',
-		       'k', 'l', 'm', 'n', 'o', 'p',
-		       'q', 'r', 's', 't', 'u', 'v',
-		       'w', 'x', 'y', 'z'};
-  char rot13key[52] = {'N', 'O', 'P', 'Q', 'R', 'S',
-		       'T', 'U', 'V', 'W', 'X', 'Y',
-		       'Z', 'A', 'B', 'C', 'D', 'E',
-		       'F', 'G', 'H', 'I', 'J', 'K',
-		       'L', 'M', 'n', 'o', 'p', 'q',
-		       'r', 's', 't', 'u', 'v', 'w',
-		       'x', 'y', 'z', 'a', 'b', 'c',
-		       'd', 'e', 'f', 'g', 'h', 'i',
-		       'j', 'k', 'l', 'm'};
+	int i, j;
+	char a[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char b[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
 
-  while (str[indx1])
-    {
-      for (indx2 = 0; indx2 < 52; indx2++)
+
+	for (i = 0; *(s + i); i++)
 	{
-	  if (str[indx1] == alphabet[indx2])
-	    {
-	      str[indx1] = rot13key[indx2];
-	      break;
-	    }
+		for (j = 0; j < 52; j++)
+		{
+			if (a[j] == *(s + i))
+			{
+				*(s + i) = b[j];
+
+				break;
+			}
+		}
 	}
-
-
-      indx1++;
-    }
-
-  return (str);
+	return (s);
 }
