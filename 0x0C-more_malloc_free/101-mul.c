f50fc98c5e63d15e6887cc90d2e602cb21aa3c39
hawau001 <hawauajoke1234@gmail.com>
2023-01-12 19:18:03 -0800

    101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -4,7 +4,6 @@
 /**
  * is_digit - checks if a string contains a non-digit char
  * @s: string to be evaluated
- *
  * Return: 0 if a non-digit is found, 1 otherwise
  */
 int is_digit(char *s)
@@ -23,7 +22,6 @@ int is_digit(char *s)
 /**
  * _strlen - returns the length of a string
  * @s: string to evaluate
- *
  * Return: the length of the string
  */
 int _strlen(char *s)
@@ -36,7 +34,6 @@ int _strlen(char *s)
 	}
 	return (i);
 }
-
 /**
  * errors - handles errors for main
  */
@@ -45,19 +42,16 @@ void errors(void)
 	printf("Error\n");
 	exit(98);
 }
-
 /**
  * main - multiplies two positive numbers
  * @argc: number of arguments
  * @argv: array of arguments
- *
- * Return: always 0 (Success)
+ *Return: always 0 (Success)
  */
 int main(int argc, char *argv[])
 {
 	char *s1, *s2;
 	int len1, len2, len, i, carry, digit1, digit2, *result, a = 0;
-
 
 	s1 = argv[1], s2 = argv[2];
 	if (argc != 3 || !is_digit(s1) || !is_digit(s2))
